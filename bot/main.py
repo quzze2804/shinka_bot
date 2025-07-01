62da5fa0ea3d7c70f25be5e34d70d3391f680311
quzze2804 <muntyan.kirill.001@gmail.com>
2025-07-01 09:38:56 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,23 +1,31 @@
 import asyncio
 import logging
+import os
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+# ⚠️ Новый способ инициализации бота
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 dp = Dispatcher()
+
 
 async def main():
     logging.basicConfig(level=logging.INFO)
-    from bot.handlers import start  # Імпортуємо стартові хендлери
+
+    from bot.handlers import start
     dp.include_router(start.router)
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
