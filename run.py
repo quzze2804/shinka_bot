f96d8a571b75c667b65f8fec74218a26032692f9
quzze2804 <muntyan.kirill.001@gmail.com>
2025-07-01 10:23:20 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,5 @@
-import sys
-import os
 import asyncio
-
-# 👇 Добавляем путь к корню проекта в sys.path
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-
-from bot.main import main
+from main import main
 
 if __name__ == "__main__":
     asyncio.run(main())
